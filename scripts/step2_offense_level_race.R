3736e65ba42351c23660c826a2919d9927a9856f
JinqianPan <60722953+JinqianPan@users.noreply.github.com>
2023-04-27 16:25:45 -0400

    step2

--- scripts/step2_offense_level_race.R
+++ scripts/step2_offense_level_race.R
@@ -31,21 +31,33 @@ offense_level_race <- complaint_data %>%
   group_by(offense_level, suspect_race) %>%
   dplyr::summarise(n = n())
 
+######
+differernt_race <- complaint_data %>%
+  filter(suspect_race != "unknown",
+         victim_race != "unknown") %>%
+  mutate(drace = 
+           ifelse(suspect_race == victim_race, "same race", "diff race")) %>%
+  select(offense_level, drace) %>%
+  group_by(offense_level, drace) %>%
+  dplyr::summarise(n = n())
+
 p1 <- ggplot(offense_level_race, aes(x=suspect_race, y=n, fill=offense_level)) + 
   geom_bar(position="stack", stat="identity") +
   ylab("The Number of Offense") + 
+  xlab("Suspect Race") +
   guides(fill=FALSE)
 
 p2 <- ggplot(offense_level_race, aes(x=suspect_race, y=n, fill=offense_level)) + 
   geom_bar(position="fill", stat="identity") +
-  ylab("The Probability of Offense")
+  ylab("The Probability of Offense") +
+  xlab("Suspect Race") +
+  guides(fill=FALSE)
+
+p3 <- ggplot(differernt_race, aes(x=drace, y=n, fill=offense_level)) + 
+  geom_bar(position="stack", stat="identity") +
+  ylab("The Number of Offense") +
+  xlab("Whether Suspect And Victim Same Race")
 
 # png(file='./figures/offense_level_race.png', width=1500, height=500, res=100)
-multiplot(p1, p2, cols=2)
+multiplot(p1, p2, p3, cols=3)
 # dev.off()
-
-
-
-
-
-
